971e88f417e47c0b7ba3cca03e20cacecfb3cf2e
Jake <jakech617@gmail.com>
2021-04-14 18:56:01 -0400

    created starting inquirer prompt with following cases for each selection

--- server.js
+++ server.js
@@ -19,3 +19,56 @@ connection.connect((err) => {
     if (err) throw err;
     init();
 });
+
+const init = () => {
+    inquirer
+        .prompt({
+            name: 'action',
+            type: 'rawlist',
+            message: 'What would you like to do?',
+            choices: [
+                'View all Employees',
+                'View all Employees by Department',
+                'View all Employees by Manager',
+                'Add Employee',
+                'Remove Employee',
+                'Update Employee Role',
+                'Update Employee Manager'
+            ],
+        })
+        .then((answer) => {
+            switch (answer.action) {
+                case 'View all Employees':
+                    employeeSearch();
+                    break;
+                
+                case 'View all Employees by Department':
+                    empDepSearch();
+                    break;
+                
+                case 'View all Employees by Manager':
+                    empManagerSearch();
+                    break;
+                
+                case 'Add Employee':
+                    addEmployee();
+                    break;
+                
+                case 'Remove Employee':
+                    removeEmployee();
+                    break;
+                
+                case 'Update Employee Role':
+                    empRoleUpdate();
+                    break;
+                
+                case 'Update Employee Manager':
+                    empManagerUpdate();
+                    break;
+                
+                default:
+                    console.log(`Invalid action: ${answer.action}`);
+                    break;
+            }
+        })
+  }
